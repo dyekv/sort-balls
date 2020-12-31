560f31d892e6e447a4763a779493b8b9e7c26f9a
dyekv <kogaktt@gmail.com>
2020-12-31 17:55:22 +0900

    add back func

--- src/App.js
+++ src/App.js
@@ -20,42 +20,42 @@ const  App = ()=> {
   const [tubes,setTubes] = useState([])
   const [picking,setPicking] = useState([])
   const [count,setCount] = useState(0)
+  const [history,setHistory] = useState([])
 
 
   const onClickTube = (targetTube)=>{
     if(picking.length === 0){ // 未選択のとき
-      console.log('未選択')
       const targetBall = tubes[targetTube].findIndex(ball=>ball > 0)
       if(targetBall === -1){return null} // 全部白（空っぽ）
       setPicking([targetTube,targetBall])
       return null
     }else{ // 選択されているとき
-      console.log('選択済み')
       const targetSpace = tubes[targetTube].filter(ball=>ball===0).length -1
+
       const moveBall = () => {
+        console.log('通った')
+        setHistory([...history,tubes.map(tube=>[...tube])])
         const replaceTubes = tubes.map(tube=>[...tube])
         replaceTubes[picking[0]].splice(picking[1],1,0)
         replaceTubes[targetTube].splice(targetSpace,1,tubes[picking[0]][picking[1]])
         setTubes(replaceTubes)
         setPicking([])
         setCount(count=>count+1)
+        console.log(history)
       }
+
       if(targetSpace === -1){ // 挿入先が満タンのとき
-        console.log('宛先満タン')
         setPicking([targetTube,0])
         return null
       }
       if(targetSpace === 3){ // 挿入先が空のとき
-        console.log('宛先空っぽ')
         moveBall()
         return null
       }
       if(tubes[targetTube][targetSpace+1] === tubes[picking[0]][picking[1]]){ // 同じ色の上に置くとき
-        console.log('宛先同色')
         moveBall()
         return null
       }
-      console.log('宛先違う色')
       setPicking([targetTube,0])
     }
   }
@@ -64,11 +64,16 @@ const  App = ()=> {
     allballs.push(...originAllballs)
     setPicking([])
     setCount(0)
+    setHistory([])
     setTubes([...Array(11)].map(()=>[...Array(4)].map(()=>selectDefaultBall())))
   }
 
   const onBack = () => {
     setCount(count => count-1)
+    const historyCopy = history.map(tubes=>tubes.map(tube=>[...tube]))
+    setTubes(historyCopy[history.length-1].map(tube=>[...tube]))
+    historyCopy.splice(historyCopy.length-1,1)
+    setHistory(historyCopy)
   }
 
   useEffect(()=>{
