db02962193bea5df1fb3babaa0cf833b2eeeb414
dyekv <kogaktt@gmail.com>
2020-12-31 14:20:40 +0900

    fix another color able to move

--- src/App.js
+++ src/App.js
@@ -14,41 +14,47 @@ const selectDefaultBall = () => {
   return selectBall
 }
 
-function App() {
+const  App = ()=> {
   const [loading,setLoading] = useState(true)
   const [tubes,setTubes] = useState([])
   const [picking,setPicking] = useState([])
 
+
   const onClickTube = (targetTube)=>{
     if(picking.length === 0){ // 未選択のとき
-      console.log(tubes[targetTube])
+      console.log('未選択')
       const targetBall = tubes[targetTube].findIndex(ball=>ball > 0)
       if(targetBall === -1){return null} // 全部白（空っぽ）
       setPicking([targetTube,targetBall])
-      console.log('選択された色は',colors[tubes[targetTube][targetBall]])
+      return null
     }else{ // 選択されているとき
+      console.log('選択済み')
       const targetSpace = tubes[targetTube].filter(ball=>ball===0).length -1
-      console.log(targetSpace)
+      const moveBall = () => {
+        const replaceTubes = tubes.map(tube=>[...tube])
+        replaceTubes[picking[0]].splice(picking[1],1,0)
+        replaceTubes[targetTube].splice(targetSpace,1,tubes[picking[0]][picking[1]])
+        setTubes(replaceTubes)
+        setPicking([])
+      }
       if(targetSpace === -1){ // 挿入先が満タンのとき
+        console.log('宛先満タン')
         setPicking([targetTube,0])
         return null
-      } 
-      const replaceTubes = tubes.map(tube=>[...tube])
-      console.log(replaceTubes)
+      }
       if(targetSpace === 3){ // 挿入先が空のとき
-        replaceTubes[picking[0]].splice(picking[1],1,0)
-        replaceTubes[targetTube].splice(targetSpace,1,tubes[picking[0]][picking[1]])
-        setTubes(replaceTubes)
-        setPicking([])
+        console.log('宛先空っぽ')
+        moveBall()
         return null
       }
-      if(tubes[targetTube][targetSpace+1] === tubes[picking[0]][picking[1]]) // 同じ色の上に置くとき
-        replaceTubes[picking[0]].splice(picking[1],1,0)
-        replaceTubes[targetTube].splice(targetSpace,1,tubes[picking[0]][picking[1]])
-        setTubes(replaceTubes)
-        setPicking([])
+      if(tubes[targetTube][targetSpace+1] === tubes[picking[0]][picking[1]]){ // 同じ色の上に置くとき
+        console.log('宛先同色')
+        moveBall()
         return null
       }
+      console.log('宛先違う色')
+      setPicking([targetTube,0])
+    }
   }
 
   useEffect(()=>{
